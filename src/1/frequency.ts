ed303eb99ac6629c50a952b9f85081cb9cfaf4dc
tvand <tvandort1@gmail.com>
2018-12-02 12:38:21 -0500

    Can find repeats really poorly.

--- src/1/frequency.ts
+++ src/1/frequency.ts
@@ -3,7 +3,46 @@ import { promisify } from 'util';
 
 const readFile = promisify(fs.readFile);
 
-const INITIAL_FREQUENCY = 0;
+const INITIAL_FREQUENCY: number = 0;
+
+export const repeatedFrequency = async (file: string): Promise<number> => {
+  const data = await readFile(file, 'utf8');
+  const lines = data.split('\n');
+  const withoutempty = lines.filter(line => Boolean(line));
+  const values = withoutempty.map(line => parseInt(line, 10));
+  const previousFrequencies: { [key: string]: number } = {
+    [INITIAL_FREQUENCY]: 1
+  };
+  let totalFrequency = INITIAL_FREQUENCY;
+  let repeat: number | null = null;
+  const collectFrequencies = (initialFrequency = INITIAL_FREQUENCY) => {
+    totalFrequency = values.reduce((previous, current) => {
+      const partialSum = current + previous;
+      const partialSumCount = previousFrequencies[partialSum];
+      if (!partialSumCount) {
+        previousFrequencies[partialSum] = 1;
+      } else {
+        previousFrequencies[partialSum] += 1;
+      }
+      if (!repeat) {
+        for (const prop of Object.keys(previousFrequencies)) {
+          const count = previousFrequencies[prop];
+          if (count === 2) {
+            repeat = parseInt(prop, 10);
+          }
+        }
+      }
+      return partialSum;
+    }, initialFrequency);
+
+    return repeat;
+  };
+  let repeatFrequency = null;
+  while (repeatFrequency === null) {
+    repeatFrequency = collectFrequencies(totalFrequency);
+  }
+  return repeatFrequency;
+};
 
 export const sumFrequencies = async (file: string) => {
   const data = await readFile(file, 'utf8');
--- src/1/test/frequency.test.ts
+++ src/1/test/frequency.test.ts
@@ -1,21 +1,37 @@
 import { join } from 'path';
-import { sumFrequencies } from '../frequency';
+import { repeatedFrequency, sumFrequencies } from '../frequency';
 
-describe('sum', () => {
-  test.each([
-    ['zero.txt', 0],
-    ['one.txt', 1],
-    ['negative-one.txt', -1],
-    ['multiple-values.txt', 8]
-  ])('read %p sum was %p', async (file, expectedSum) => {
-    expect(await sumFrequencies(join(__dirname, file))).toBe(expectedSum);
+describe('frequency', () => {
+  describe(sumFrequencies, () => {
+    test.each([
+      ['zero.txt', 0],
+      ['one.txt', 1],
+      ['negative-one.txt', -1],
+      ['multiple-values.txt', 8]
+    ])('read %p sum was %p', async (file, expectedSum) => {
+      expect(await sumFrequencies(join(__dirname, file))).toBe(expectedSum);
+    });
+
+    it('throws when file does not exist', async () => {
+      try {
+        await sumFrequencies('does_not_exist.txt');
+      } catch (error) {
+        expect(error.code).toMatchSnapshot();
+      }
+    });
   });
 
-  it('throws when file does not exist', async () => {
-    try {
-      await sumFrequencies('does_not_exist.txt');
-    } catch (error) {
-      expect(error.code).toMatchSnapshot();
-    }
+  describe(repeatedFrequency, () => {
+    it('read repeated-zero.txt and detected repeated zero', async () => {
+      expect(
+        await repeatedFrequency(join(__dirname, 'repeated-zero.txt'))
+      ).toBe(0);
+    });
+
+    it('reads ten-twice.txt and detected repeated ten', async () => {
+      expect(await repeatedFrequency(join(__dirname, 'ten-twice.txt'))).toBe(
+        10
+      );
+    });
   });
 });
