dbb5a7a21b3a71a29f22e32fd8c58c3f11f6fdc0
tvand <tvandort1@gmail.com>
2018-12-02 03:14:29 -0500

    Add tslint
    Remove eslint

--- src/1/frequency.ts
+++ src/1/frequency.ts
@@ -9,7 +9,7 @@ export default async (file: string) => {
   const data = await readFile(file, 'utf8');
   const lines = data.split('\n');
   const withoutEmpty = lines.filter(line => Boolean(line));
-  const values = withoutEmpty.map(line => parseInt(line));
+  const values = withoutEmpty.map(line => parseInt(line, 10));
   const sum = values.reduce(
     (previous, current) => current + previous,
     INITIAL_FREQUENCY
--- src/index.test.ts
+++ src/index.test.ts
@@ -2,12 +2,9 @@ import { join } from 'path';
 import * as frequency from './1/frequency';
 
 let frequencyDefaultSpy: jest.SpyInstance<(file: string) => Promise<number>>;
-let consoleLogSpy: jest.SpyInstance<
-  (message?: any, ...optionalParams: any[]) => void
->;
 beforeAll(() => {
   frequencyDefaultSpy = jest.spyOn(frequency, 'default');
-  consoleLogSpy = jest.spyOn(console, 'log');
+  jest.spyOn(console, 'log');
 });
 
 beforeEach(() => {
@@ -29,6 +26,7 @@ describe('index', () => {
 
   it('will call frequency default', () => {
     expect(frequency.default).toBeCalledWith(join(__dirname, './1/input.txt'));
+    // tslint:disable-next-line:no-console
     expect(console.log).toBeCalledWith('Total frequency: ', 101);
   });
 });
