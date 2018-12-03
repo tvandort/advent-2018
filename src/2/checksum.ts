d6e1feff65b84a2c0dbf5a09406b868f68078117
tvand <tvandort1@gmail.com>
2018-12-02 21:11:02 -0500

    WIP

--- src/2/checksum.ts
+++ src/2/checksum.ts
@@ -1,29 +1,8 @@
 import { readCleanLines } from 'fs-util';
 
-export const countCharacters = (input: string) => {
-  const characters = input.split('');
-  const characterCounts = characters.reduce(
-    (counts: Map<string, number>, character: string) => {
-      if (counts.has(character)) {
-        const oldCount = counts.get(character) as number;
-        counts.set(character, oldCount + 1);
-      } else {
-        counts.set(character, 1);
-      }
-      return counts;
-    },
-    new Map<string, number>()
-  );
-  const countCounts = Array.from(characterCounts.values()).reduce(
-    (counts: Set<number>, value: number) => {
-      if (value > 1) {
-        counts.add(value);
-      }
-      return counts;
-    },
-    new Set<number>()
-  );
-  return Array.from(countCounts).sort();
+export const checksumFromFile = async (file: string) => {
+  const data = await readCleanLines(file);
+  return checksum(data);
 };
 
 export const checksum = (input: string[]) => {
@@ -31,20 +10,50 @@ export const checksum = (input: string[]) => {
   return Array.from(
     initialArray
       .concat(...input.map(countCharacters))
-      .reduce((countMap: Map<number, number>, value: number) => {
-        if (countMap.has(value)) {
-          const count = countMap.get(value) as number;
-          countMap.set(value, count + 1);
-        } else {
-          countMap.set(value, 1);
-        }
-        return countMap;
-      }, new Map<number, number>())
+      .reduce(countEachInstanceOfNumber, new Map<number, number>())
       .values()
   ).reduce((accumulator, current) => accumulator * current, 1);
 };
 
-export const checksumFromFile = async (file: string) => {
-  const data = await readCleanLines(file);
-  return checksum(data);
+export const countCharacters = (input: string) => {
+  const characters = input.split('');
+  const characterCounts = characters.reduce(
+    countEachCharacterInString,
+    new Map<string, number>()
+  );
+  const countCounts = Array.from(characterCounts.values()).reduce(
+    uniqueSetOfNumbers,
+    new Set<number>()
+  );
+  return Array.from(countCounts).sort();
+};
+
+function countEachCharacterInString<T>(counts: Map<T, number>, character: T) {
+  if (counts.has(character)) {
+    const oldCount = counts.get(character) as number;
+    counts.set(character, oldCount + 1);
+  } else {
+    counts.set(character, 1);
+  }
+  return counts;
+}
+
+const countEachInstanceOfNumber = (
+  countMap: Map<number, number>,
+  value: number
+) => {
+  if (countMap.has(value)) {
+    const count = countMap.get(value) as number;
+    countMap.set(value, count + 1);
+  } else {
+    countMap.set(value, 1);
+  }
+  return countMap;
+};
+
+const uniqueSetOfNumbers = (counts: Set<number>, value: number) => {
+  if (value > 1) {
+    counts.add(value);
+  }
+  return counts;
 };
