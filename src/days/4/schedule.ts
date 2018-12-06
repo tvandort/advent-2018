12b763774381f2a33b1af7458941c1c8299d6426
Tom <tvandort1@gmail.com>
2018-12-05 22:08:52 -0500

    WIP

--- src/days/4/schedule.ts
+++ src/days/4/schedule.ts
@@ -1,17 +1,28 @@
 import moment from 'moment';
 
-class ScheduleEvent {
-  public OccurredOn: moment.Moment;
+export class TimelineEvent {
+  public OccurredOn: Date;
+  public Event: string;
 
-  constructor(occurredOn: moment.Moment) {
-    this.OccurredOn = occurredOn;
+  constructor(occurredOn: moment.Moment, event: string) {
+    this.OccurredOn = occurredOn.toDate();
+    this.Event = event;
+  }
+}
+
+export class Timeline {
+  public Events: any;
+  constructor(events: TimelineEvent[]) {
+    this.Events = events.sort((a, b) =>
+      moment(a.OccurredOn).diff(moment(b.OccurredOn))
+    );
   }
 }
 
 export const readScheduleLine = (scheduleLine: string) => {
   const dateTimeString = scheduleLine.slice(0, 18).trim();
-  const date = moment(dateTimeString, 'YYYY-MM-DD hh:mm').toDate();
+  const date = moment(dateTimeString, 'YYYY-MM-DD HH:mm');
   const eventString = scheduleLine.slice(19).trim();
 
-  return { time: date, event: eventString };
+  return new TimelineEvent(date, eventString);
 };
--- src/days/4/test/schedule.test.ts
+++ src/days/4/test/schedule.test.ts
@@ -1,17 +1,60 @@
-import { readScheduleLine } from '../schedule';
+import { readScheduleLine, TimelineEvent, Timeline } from '../schedule';
+import moment from 'moment';
+
+const timelineEvent1 = new TimelineEvent(
+  moment({
+    year: 1518,
+    month: 10,
+    day: 1,
+    hours: 0,
+    minutes: 0
+  }),
+  'Guard #10 begins shift'
+);
+const timelineEvent2 = new TimelineEvent(
+  moment({
+    year: 1518,
+    month: 10,
+    day: 1,
+    hours: 0,
+    minutes: 5
+  }),
+  'falls asleep'
+);
+
+const timelineEvent3 = new TimelineEvent(
+  moment({
+    year: 1518,
+    month: 11,
+    day: 1,
+    hours: 0,
+    minutes: 1
+  }),
+  'Guard #11 begins shift'
+);
 
 describe(readScheduleLine, () => {
   it('splits date and event', () => {
-    const time = new Date(1518, 10, 1, 0, 0);
-    const event = 'Guard #10 begins shift';
     const readString = '[1518-11-01 00:00] Guard #10 begins shift';
-    expect(readScheduleLine(readString)).toEqual({ time, event });
+    expect(readScheduleLine(readString)).toEqual(timelineEvent1);
   });
 
   it('splits date and event', () => {
-    const time = new Date(1518, 10, 1, 0, 5);
-    const event = 'falls asleep';
     const readString = '[1518-11-01 00:05] falls asleep';
-    expect(readScheduleLine(readString)).toEqual({ time, event });
+    expect(readScheduleLine(readString)).toEqual(timelineEvent2);
+  });
+
+  it('can construct a timeline', () => {
+    const timeline = new Timeline([
+      timelineEvent2,
+      timelineEvent1,
+      timelineEvent3
+    ]);
+
+    expect(timeline.Events).toEqual([
+      timelineEvent1,
+      timelineEvent2,
+      timelineEvent3
+    ]);
   });
 });
