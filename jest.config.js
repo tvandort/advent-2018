4ca5c0c764b26452d218c1a4adf3b6ee1102a1c0
Tom <tom@vandort.dev>
2019-07-05 08:36:06 -0400

    Update all packages and how they works.

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   collectCoverageFrom: ['**/src/**/*.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/src/index.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  setupTestFrameworkScriptFile: '<rootDir>/test/setup-test-framework.ts',
+  setupFilesAfterEnv: ['<rootDir>/test/setup-test-framework.ts'],
   moduleDirectories: ['node_modules', '<rootDir>/src/util']
 };
